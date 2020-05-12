e8b6d5dce28d3c6807ef0084628cbc9f8ddd0140
JustinF97 <64065932+JustinF97@users.noreply.github.com>
2020-05-12 17:23:28 +0200

    Update Krankenhaus.py

--- Krankenhaus.py
+++ Krankenhaus.py
@@ -7,3 +7,24 @@ class Krankenhaus:
 		self.verfuegbarkeit = verfuegbarkeit
 		
 m_krankenhaus = []
+
+def on_connect(client, userdata, flags, rc):
+	client.subscribe(["hshl/krankenhaus/krankenhaus1, 2)])
+			  
+def on_message(client, userdata, msg):
+	print(str(msg.payload))
+			
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+			 
+client.connect("test.mosquitto.org, 1883, 60)
+	       
+data = {
+    "name": "Krankenhaus 1", 
+    "verfuegbarkeit" :["Platz frei", "Kein Platz frei"], 
+    "topic": "hshl/krankenhaus/krankenhaus1"
+    }
+client.publish("hshl/krankenhaus/krankenhaus1", json.dumps(data))
+
+client.loop_forever()
