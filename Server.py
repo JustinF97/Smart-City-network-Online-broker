708e52c649f51bf8ae94cc8d620bb9040fc94e27
JustinF97 <64065932+JustinF97@users.noreply.github.com>
2020-05-13 10:26:06 +0200

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,1 +1,37 @@
+import paho.mqtt.client as mqtt
+import json
 
+class Krankenhaus:
+	def _init_(self, auswahl, verfuegbarkeit): 
+		self.auswahl = auswahl
+		self.verfuegbarkeit = verfuegbarkeit
+		
+m_krankenhaus = []
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe([
+                    ("krankenhaus/server/order", 2), 
+                    ("krankenhaus/server/krankenhaus1", 2),
+                    ])
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    if(msg.topic.endswith('krankenhaus1')):
+        register_company(msg.payload)
+    if(msg.topic.endswith('order')):
+        process_order(msg.payload)
+        
+def process_order(data):
+    js = json.loads(data)
+    name = js['name']
+    adresse = js['adresse']
+    platz = js['platz']
+    narricht = js['narricht']
+    
+    selected_krankenhaus = None
+    
+    for krankenhaus in m_krankenhaus
+     if adresse in krankenhaus.ort:
+        selected_krankenhaus = krankenhaus
+    
